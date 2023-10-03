054a55a944e72bda472171070ca1df64d8be5b3e
Aron Frysztacki <aron.frysztacki24@gmail.com>
2023-10-03 21:09:24 +0200

    created new repository

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar left &
+polybar center &
+polybar right & disown 
+
+
+echo "Polybar launched..."
